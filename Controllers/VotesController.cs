d689de8cc1414231f8bc980aee94cff2aa76e603
Lucas Guimarães <lucas.guimaraes@plugntrade.com.br>
2025-04-21 22:20:29 -0300

    Add controllers

--- Controllers/VotesController.cs
+++ Controllers/VotesController.cs
@@ -1,6 +1,25 @@
-﻿namespace voting_api.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace voting_api.Controllers
 {
     public class VotesController
     {
+
+        [HttpGet]
+        [Route("api/votes")]
+
+        public IActionResult GetVotes()
+        {
+            // Logic to get votes
+            return Ok(new { message = "Votes retrieved successfully" });
+        }
+
+        [HttpPost]
+        [Route("api/votes")]
+        public IActionResult CreateVote([FromBody] Vote vote)
+        {
+            // Logic to create a vote
+            return CreatedAtAction(nameof(GetVotes), new { id = vote.Id }, vote);
+        }
     }
 }
